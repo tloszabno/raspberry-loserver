1f03f5fbea76c804d62466f366963e07f7cc395c
Tomasz Łoś <tloszabno@gmail.com>
2017-02-04 09:14:46 +0100

    losserver: added simple test

--- tests/test_humidex_slo.py
+++ tests/test_humidex_slo.py
@@ -2,9 +2,19 @@ import unittest
 from hamcrest import *
 from mockito import *
 from loserver import humidex_slo
-from loserver import devices
 
 
 class TestHumidexSLO_shallow_cache(unittest.TestCase):
-    def test_should_append_data_to_shallow_cache_in_adding_order(self):
-        assert_that(True, equal_to(True))
+    def test_should_load_first_data_on_init_to_shallow_cache(self):
+        # given
+        humidexDevicesFacade = mock()
+        when(humidexDevicesFacade).get_humidex_indoor().thenReturn(
+            (25.0, 20.0))
+        when(humidexDevicesFacade).get_humidex_outdoor().thenReturn(
+            (2.0, 80.0))
+
+        # when
+        humidex = humidex_slo.HumidexSLO(humidexDevicesFacade)
+
+        # then
+        assert_that(len(humidex.shallow_cache), equal_to(1))
